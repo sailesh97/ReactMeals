c73d97200750e024f87627875dd8d99674883c7f
sailesh97 <saileshpatra997@gmail.com>
2022-10-08 01:26:03 +0530

    Check

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,42 +1,42 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    }
-];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => (
-        <MealItem 
+    const { REACT_APP_API_ENDPOINT: getMealsURI } = process.env;
+
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        const fetchMeals = async () => {
+            console.log("URL--",getMealsURI)
+            const response = await fetch(`${getMealsURI}`);
+            const responseData = await response.json();
+            console.log("Response Data--", responseData)
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                });
+            }
+            setMeals(loadedMeals);
+        };
+
+        fetchMeals();
+    }, [getMealsURI])
+
+    const mealsList = meals.map(meal => (
+        <MealItem
             id={meal.id}
-            key={meal.id} 
+            key={meal.id}
             name={meal.name}
             description={meal.description}
             price={meal.price}
-            />
+        />
     ));
     return <section className={classes.meals}>
         <Card>
