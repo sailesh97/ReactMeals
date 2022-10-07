b5fd91dd5bf639b3913af1f6cf57fb5600f36951
sailesh97 <saileshpatra997@gmail.com>
2022-10-07 19:57:29 +0530

    Cart Badge in Navbar updating on clicking Add Button in MealsForm component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,30 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amout" input={{
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amaountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amaountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    }
+
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input 
+            ref={amaountInputRef}
+            label="Amount" 
+            input={{
             id: 'amount_' + props.id,
             type: 'number',
             min: '1',
@@ -12,6 +33,7 @@ const MealItemForm = props => {
             defaultValue:'1'
          }}/>        
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
 }
 
