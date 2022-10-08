1adb8e9f8ea1bd648391ec629230b7c82bbb9754
sailesh97 <saileshpatra997@gmail.com>
2022-10-09 01:24:34 +0530

    Issue fixed

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -28,7 +28,7 @@ const MealItemForm = props => {
             id: 'amount_' + props.id,
             type: 'number',
             min: '1',
-            max: '1',
+            max: '5',
             step: '1',
             defaultValue:'1'
          }}/>        
